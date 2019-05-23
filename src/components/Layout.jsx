269209a2f70e6b8c6763da84791e98cc5fce6f8d
triste <ultima.machina@gmail.com>
2019-05-22 19:59:58 -0700

    Content wrapper CSS for larger breakpoints

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
-import { withTheme } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import NavBar from './NavBar'
 import Grid from '@material-ui/core/Grid';
 
-const Layout = ({
-  theme,
-  children
-}) => {
-  const styles = {
-    layout: {
-      backgroundColor: theme.palette.background.default,
-      overflow: 'hidden'
+const styles = (theme) => ({
+  layout: {
+    backgroundColor: theme.palette.background.default,
+    overflow: 'hidden'
+  },
+  contentWrapper: {
+    [theme.breakpoints.down('xl')]: {
+      maxWidth: 1300,
+      margin: '0 auto'
     }
   }
+});
+const Layout = ({
+  theme,
+  children,
+  classes,
+}) => {
   return(
-    <Grid container style={styles.layout}>
+    <Grid container className={classes.layout}>
       <Grid item xs={12}>
         <NavBar />
       </Grid>
       <Grid item xs={12}>
-        {children}
+        <div className={classes.contentWrapper}>
+          {children}
+        </div>
       </Grid>
     </Grid>
   )
 }
-export default withTheme()(Layout);
+export default withStyles(styles)(Layout);
