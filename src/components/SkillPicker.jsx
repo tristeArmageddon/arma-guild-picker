c9ec6c2ba1a3117a72cb5a9fcdcda41ae0574338
triste <ultima.machina@gmail.com>
2019-05-27 08:28:41 -0700

    Render matching guilds, clickable as links

--- src/components/SkillPicker.jsx
+++ src/components/SkillPicker.jsx
@@ -5,6 +5,8 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import data from '../data/data.json';
 import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
 
 const styles = (theme) => ({
   sectionTitle: {
@@ -16,6 +18,12 @@ const styles = (theme) => ({
     padding: 20,
     textTransform: 'capitalize',
   },
+  guildOptionText: {
+    padding: '0 20px',
+  },
+  guildOptionLink: {
+    textDecoration: 'none',
+  },
   selectedCount: {
     color: '#ffffff5e'
   },
@@ -24,17 +32,53 @@ const styles = (theme) => ({
   },
   chip: {
     margin: '0 4px 4px 0',
+  },
+  clearSelected: {
+    float: 'right',
   }
 });
 class SkillPicker extends Component {
   state = {
     selectedSkills: [],
+    guilds: [],
+    subguilds: [],
+  }
+
+  componentWillMount() {
+    for (let guildGroup of ['guilds', 'subguilds']) {
+      const guilds = data[guildGroup] ? Object.keys(data[guildGroup]) : [];
+      const result = [];
+      for (let guild of guilds) {
+        result.push({
+          key: guild,
+          label: data[guildGroup][guild].label,
+          skills: this.flattenGuild(guildGroup, guild),
+          karma: data[guildGroup][guild].karma,
+        });
+      }
+      this.setState({
+        [guildGroup]: result
+      })
+    }
+  }
+
+  flattenGuild = (guildGroup, guild) => {
+    let result = [];
+    for (let skillPrefix of ['', 'ex']) {
+      for (let profLevel of ['languages', ...data.proficiencies]) {
+        if (data[guildGroup][guild][`${skillPrefix}${profLevel}`]) {
+          result = [...data[guildGroup][guild][`${skillPrefix}${profLevel}`], ...result]
+        }
+      }
+    }
+    return result;
   }
 
   toggleSelection = (event, skill) => {
     const {
       selectedSkills
     } = this.state;
+
     const itemIndex = selectedSkills.indexOf(skill)
     if (itemIndex === -1) {
       this.setState({ selectedSkills: [skill, ...selectedSkills]})
@@ -44,8 +88,31 @@ class SkillPicker extends Component {
     }
   }
 
-  findGuildsForSkills() {
+  clearSelection = () => {
+    this.setState({ selectedSkills: [] })
+  }
 
+  findGuildsForSkills = () => {
+    const result = [];
+    for (let guild of this.state.guilds) {
+      for (let subguild of this.state.subguilds) {
+        let skillsMatched = 0;
+        for (let skill of this.state.selectedSkills) {
+          if (guild.skills.includes(skill) || subguild.skills.includes(skill)) {
+            skillsMatched++;
+          }
+        }
+
+        if (skillsMatched === this.state.selectedSkills.length) {
+          const karmaLabel = (guild.karma || subguild.karma) ? ` (${guild.karma + subguild.karma})` : '';
+          result.push({
+            label: `${guild.label}/${subguild.label}${karmaLabel}`,
+            linkTo: `/arma-guild-picker/?guild=${guild.key}&subguild=${subguild.key}`
+          })
+        }
+      }
+    }
+    return result;
   }
 
   render () {
@@ -59,12 +126,25 @@ class SkillPicker extends Component {
       <Grid container justify="center" spacing={16} className={classes.container}>
         <Grid item xs={12} sm={6}>
           <Paper square>
-            <Typography className={classes.sectionTitle}>
-              Skills
-              {' '}
-              <span className={classes.selectedCount}>({selectedSkills.length} selected)</span>
-            </Typography>
-            {data.skillGroups.map(skillGroup => (
+            <Grid container>
+              <Grid item xs={12} sm={6}>
+                <Typography className={classes.sectionTitle}>
+                  Skills
+                  {' '}
+                  <span className={classes.selectedCount}>({selectedSkills.length} selected)</span>
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                {selectedSkills.length > 0 && (<Button
+                  size="small"
+                  className={classes.clearSelected}
+                  onClick={() => this.clearSelection()}
+                >
+                  Clear
+                </Button>)}
+              </Grid>
+              </Grid>
+            {['languages', ...data.skillGroups].map(skillGroup => (
               <>
                 <Typography className={classes.skillGroupTitle}>{skillGroup}</Typography>
                 <div className={classes.chipContainer}>
@@ -83,7 +163,22 @@ class SkillPicker extends Component {
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper square>
-            <Typography className={classes.sectionTitle}>Matching Guild Combinations</Typography>
+            <Typography className={classes.sectionTitle}>
+              Matching Guild
+              {' '}
+              <span className={classes.selectedCount}>(Click to View in Guild Picker)</span>
+            </Typography>
+            <Grid container justify="center" spacing={16} className={classes.container}>
+              {this.findGuildsForSkills().map(guildCombination => (
+                <Grid item xs={12} sm={6} className={classes.guildOption}>
+                  <Link to={guildCombination.linkTo} className={classes.guildOptionLink}>
+                    <Typography className={classes.guildOptionText}>
+                      {guildCombination.label}
+                    </Typography>
+                  </Link>
+                </Grid>
+              ))}
+            </Grid>
           </Paper>
         </Grid>
       </Grid>
