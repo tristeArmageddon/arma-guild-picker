ee9d19485372640dd76e17240777f93b1fba55ae
triste <ultima.machina@gmail.com>
2019-06-20 06:26:18 -0700

    Begin to build out table for rendering player race data

---
+++ src/components/RaceTable.jsx
@@ -0,0 +1,89 @@
+import React, { Component }from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import data from '../data/data.json'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = (theme) => ({
+  cell: {
+    width: '50%',
+    [theme.breakpoints.down('xs')]: {
+      padding: 4,
+      fontSize: '0.65rem',
+      maxWidth: 65,
+      paddingRight: '4px !important'
+    },
+    [theme.breakpoints.down('sm')]: {
+      padding: 8,
+    },
+  },
+  noContent: {
+    padding: '1rem',
+    textAlign: 'center',
+    width: '100%'
+  }
+});
+class RaceTable extends Component {
+  render() {
+    const {
+      classes,
+    } = this.props;
+
+    const selectedRace = data.races.human;
+
+    return (
+      <Paper square className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.cell}>
+                {selectedRace.label}
+              </TableCell>
+              <TableCell className={classes.cell}>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={selectedRace.documentation}
+                >
+                  Documentation
+                </a>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow key="karma">
+              <TableCell className={classes.cell} component="th" scope="row">
+                Karma Cost
+              </TableCell>
+              <TableCell className={classes.cell}>
+                {selectedRace.karma}
+              </TableCell>
+            </TableRow>
+            <TableRow key="heightmin">
+              <TableCell className={classes.cell} component="th" scope="row">
+                Minimum Height (inches)
+              </TableCell>
+              <TableCell className={classes.cell}>
+                {selectedRace.heightmin}
+              </TableCell>
+            </TableRow>
+            <TableRow key="heightmax">
+              <TableCell className={classes.cell} component="th" scope="row">
+                Maximum Height (inches)
+              </TableCell>
+              <TableCell className={classes.cell}>
+                {selectedRace.heightmax}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+}
+export default withStyles(styles)(RaceTable);
+
