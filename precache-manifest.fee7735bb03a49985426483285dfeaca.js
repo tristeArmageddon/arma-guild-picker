9ce60074d2d4a2b2c5f9d041095756c1f544b9bf
triste <ultima.machina@gmail.com>
2019-12-15 17:41:20 -0800

    Updates

--- precache-manifest.fee7735bb03a49985426483285dfeaca.js
+++ precache-manifest.fee7735bb03a49985426483285dfeaca.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef04867f9fdf8cc63623647568509bda",
+    "revision": "8bdeac774ca1460fbcd1be05e53a042c",
     "url": "/arma-guild-picker/index.html"
   },
   {
-    "revision": "8ca3f095e406f4871c39",
+    "revision": "23cb0ff0f1c02a43d950",
     "url": "/arma-guild-picker/static/css/main.6dcaecb9.chunk.css"
   },
   {
-    "revision": "1abc4c5edd123d5e74e9",
-    "url": "/arma-guild-picker/static/js/2.546821c9.chunk.js"
+    "revision": "d22303837e40107f7c16",
+    "url": "/arma-guild-picker/static/js/2.cc5ab260.chunk.js"
   },
   {
-    "revision": "8ca3f095e406f4871c39",
-    "url": "/arma-guild-picker/static/js/main.30f8b2d4.chunk.js"
+    "revision": "23cb0ff0f1c02a43d950",
+    "url": "/arma-guild-picker/static/js/main.a3508735.chunk.js"
   },
   {
     "revision": "02fdbe61caf0c1037a93",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arma-guild-picker/precache-manifest.8ce5d6d80019960d1914518b414ab22d.js"
+  "/arma-guild-picker/precache-manifest.fee7735bb03a49985426483285dfeaca.js"
 );
 
 self.addEventListener('message', (event) => {
