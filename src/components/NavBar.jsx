926de1fe0a71dc4fecc484d9883da4ece1aa27c2
triste <ultima.machina@gmail.com>
2021-09-15 18:11:09 -0700

    Package update and add README.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,6 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Icon from '@material-ui/core/Icon';
 import { NavLink } from "react-router-dom";
 import { withStyles } from '@material-ui/core/styles';
 import { withRouter } from "react-router";
